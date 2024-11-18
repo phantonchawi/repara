af6130188653b829af757eee668ff64fdeff05cc
phantonchawi <chawimartinez5528@gmail.com>
2024-11-18 17:07:26 -0600

    reparadora sitio web

---
+++ script.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+document.getElementById('form-contacto').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+    if (nombre && email && mensaje) {
+        alert(`Gracias, ${nombre}, por tu mensaje. Nos pondremos en contacto contigo.`);
+        
+    } else {
+        alert('Por favor, rellena todos los campos.');
+    }
+});
